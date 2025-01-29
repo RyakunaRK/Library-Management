eddde85b85c9d8a179877ca219dba91af8523801
Jhonas Gomes <jhowscts@gmail.com>
2025-01-29 16:25:24 -0300

    Add project files.

---
+++ Entities/Livro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management.Entities
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get ; set; }
+        public string Autor { get; set; }
+        public string ISBN { get; set; }
+        public int AnoDePublicacao { get; set; }
+
+        public Livro(int id, string titulo, string autor, string isbn, int ano)
+        {
+            this.Id = id;
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.ISBN = isbn;
+            this.AnoDePublicacao = ano;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
